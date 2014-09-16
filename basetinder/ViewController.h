24234f78ac842e66ac279d6aef2bf8f6e125013e
tuyo1009 <tuyo1009@gmail.com>
2014-09-16 15:03:02 +0900

    Initial Commit

---
+++ basetinder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  basetinder
+//
+//  Created by EndoTsuyoshi on 2014/09/16.
+//  Copyright (c) 2014年 in.thebase. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ basetinder/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  basetinder
+//
+//  Created by EndoTsuyoshi on 2014/09/16.
+//  Copyright (c) 2014年 in.thebase. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
